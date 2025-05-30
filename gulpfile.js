546f43e6c9d2770a3322aa69c30f658efe88efd3
RaiHammer <arta-daniil@ya.ru>
2025-05-30 19:40:22 +0300

    prepared for publish

--- gulpfile.js
+++ gulpfile.js
@@ -2,51 +2,7 @@ const gulp = require('gulp');
 const concat = require('gulp-concat-css');
 const plumber = require('gulp-plumber');
 const del = require('del');
-const browserSync = require('browser-sync').create(); 
-
-function html() {
-  return gulp.src('src/**/*.html') // тут нам нужно описать, что делает функция
-        .pipe(plumber())
-              .pipe(gulp.dest('dist/'))
-              .pipe(browserSync.reload({stream: true}));
-}
-
-exports.html = html // строчка, которая позволит вызвать эту задачу из терминала 
-
-function css() {
-  return gulp.src('src/blocks/**/*.css')
-        .pipe(plumber())
-        .pipe(concat('bundle.css'))
-                .pipe(gulp.dest('dist/'))
-                .pipe(browserSync.reload({stream: true}));
-}
-
-exports.css = css; 
-
-function images() {
-  return gulp.src('src/images/**/*.{jpg,png,svg,gif,ico,webp,avif}')
-            .pipe(gulp.dest('dist/images'))
-            .pipe(browserSync.reload({stream: true}));
-}
-
-exports.images = images; 
-
-function clean() {
-  return del('dist');
-}
-
-exports.clean = clean;
-
-const build = gulp.series(clean, gulp.parallel(html, css, images));
-
-exports.build = build; 
-
-function watchFiles() {
-  gulp.watch(['src/**/*.html'], html);
-  gulp.watch(['src/blocks/**/*.css'], css);
-  gulp.watch(['src/images/**/*.{jpg,png,svg,gif,ico,webp,avif}'], images);
-  
-}
+const browserSync = require('browser-sync').create();
 
 function serve() {
   browserSync.init({
@@ -54,12 +10,47 @@ function serve() {
       baseDir: './dist'
     }
   });
-} 
+}
 
+function html() {
+  return gulp.src('src/**/*.html')
+        .pipe(plumber())
+				.pipe(gulp.dest('dist/'))
+        .pipe(browserSync.reload({stream: true}));
+}
 
-const watchapp = parallel(build, watchFiles, serve); 
+function css() {
+  return gulp.src('src/blocks/**/*.css')
+        .pipe(plumber())
+        .pipe(concat('bundle.css'))
+				.pipe(gulp.dest('dist/'))
+        .pipe(browserSync.reload({stream: true}));
+}
 
+function images() {
+  return gulp.src('src/images/**/*.{jpg,png,svg,gif,ico,webp,avif}')
+    .pipe(gulp.dest('dist/images'))
+    .pipe(browserSync.reload({stream: true}));
+}
+
+function clean() {
+  return del('dist');
+}
+
+function watchFiles() {
+  gulp.watch(['src/**/*.html'], html);
+  gulp.watch(['src/blocks/**/*.css'], css);
+  gulp.watch(['src/images/**/*.{jpg,png,svg,gif,ico,webp,avif}'], images);
+}
+
+const build = gulp.series(clean, gulp.parallel(html, css, images));
+const watchapp = gulp.parallel(build, watchFiles, serve);
+
+exports.html = html;
+exports.css = css;
+exports.images = images;
+exports.clean = clean;
+
+exports.build = build;
 exports.watchapp = watchapp;
-exports.default = watchapp; 
-
-
+exports.default = watchapp;
